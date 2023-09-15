ca84974c47cadc868d4448000e75447db9c3914a
AIWASS23 <marcelodearaujo677@gmail.com>
2023-09-15 14:21:05 -0300

    Initial Commit

---
+++ Weald/WealdApp.swift
@@ -0,0 +1,17 @@
+//
+//  WealdApp.swift
+//  Weald
+//
+//  Created by Marcelo De Araújo on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WealdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
